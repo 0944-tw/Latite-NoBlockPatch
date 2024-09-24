8b55efa3cab818dd1b9287ba49b87c8045805e12
zeroninefourfour <zwec181818@proton.me>
2024-09-24 19:04:43 +0800

    Remove blockModules Functions Content

--- src/client/Latite.cpp
+++ src/client/Latite.cpp
@@ -505,32 +505,7 @@ void Latite::patchKey() {
 
 
 static void blockModules(std::string_view moduleName, std::string_view serverName) {
-    auto inst = SDK::RakNetConnector::get();
-
-    std::vector<std::wstring> blockedList;
-    if (inst->dns.find(serverName) != std::string::npos) {
-        Latite::getModuleManager().forEach([&](std::shared_ptr<IModule> mod) {
-            if (!mod->isBlocked()) {
-                if (mod->name() == moduleName) {
-                    blockedList.push_back(mod->getDisplayName());
-                    mod->setBlocked(true);
-                }
-            }
-            });
-    }
-
-    if (!blockedList.empty()) {
-        std::wstring str;
-        for (size_t i = 0; i < blockedList.size(); i++) {
-            str += blockedList[i];
-            if (i != blockedList.size() - 1) {
-                str += L", ";
-            }
-        }
-        str += L" will be blocked on this server.";
-
-        Latite::getNotifications().push(str);
-    }
+   
 }
 
 void Latite::updateModuleBlocking() {
